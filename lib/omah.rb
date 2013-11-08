795f00c6a48a14299784607acfbb2202e897991f
James <james@ralina.(none)>
2013-11-08 20:17:52 +0000

    feature: email messages are now saved to individual files

--- lib/omah.rb
+++ lib/omah.rb
@@ -7,7 +7,10 @@ require 'dynarex-daily'
 
 class Omah
 
-  def initialize()
+  def initialize(options={})
+
+    opt = {user: ''}.merge options
+    @user = opt[:user]
 
     @dd = DynarexDaily.new 
     @dd.schema = 'messages/message(id, from, to, subject, date, body_text, body_html)'
@@ -15,7 +18,31 @@ class Omah
 
   def store messages
 
-    messages.each {|message| @dd.create message }
+    dynarex = Dynarex.new 'messages/message(id, title, txt_file, html_file)'
+
+    messages.each do |msg| 
+
+      #puts 'msg[:subject] : ' + msg.subject.inspect
+      subject = msg[:subject]
+      title = subject.gsub(/\W+/,'-')[0,30].sub(/-$/,'')
+      a = dynarex.find_all_by_title subject
+      ordinal = a.any? ? '.' + a.length.to_s : ''
+      txt_file = title + ordinal + '.txt'
+      html_file = title + ordinal + '.html'
+
+      id = msg[:id]
+      next if dynarex.find_by_id id
+
+      dynarex.create id: id, title: subject, txt_file: txt_file, 
+                  html_file: html_file
+      @dd.create msg
+      path = File.join @user + '/inbox' 
+      FileUtils.mkdir_p path
+      File.write File.join(path, txt_file), msg[:body_text]
+      File.write File.join(path, html_file), msg[:body_html]
+    end
+
+    dynarex.save 'dynarex.xml'
     @dd.save
   end
 
